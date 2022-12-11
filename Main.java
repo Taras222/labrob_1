12218e363e806e10eb9d4121f368e4f4dc12b2c3
Taras222 <99286172+Taras222@users.noreply.github.com>
2022-12-11 12:09:22 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Planet planet = new Planet("Kepler", 2, 4, true);
+        Satellite satellite = new Satellite("Carme", 175, 65);
+        Star star = new Star("Alpha Centavra", 430, 670);
+
+        planet.suitableForLife();
+        System.out.println("Планета " + planet.name + " буде жити ще " + planet.cycleLife() + " млрд. років");
+        satellite.showTides();
+        star.clasification();
+        System.out.println("Зірка " + star.name + " буде світити ще " + star.starBright() + " млрд. років");
+    }
+}
